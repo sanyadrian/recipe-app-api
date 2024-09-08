45012d06819f7800f73d467a1523d2eef6c4be36
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-08 15:44:34 -0400

    test for create user page in admin panel and customization of user creation in admin panel

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,3 +32,8 @@ class AdminSiteTests(TestCase):
         url = reverse("admin:core_user_change", args=[self.user.id])
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
+    
+    def test_create_user_page(self):
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
