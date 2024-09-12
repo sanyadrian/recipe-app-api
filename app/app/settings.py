a96963de25be434628f013de50d5da5debd80062
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-12 11:25:29 -0400

    Testing Recipe API

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "user",
     "drf_spectacular",
     "rest_framework.authtoken",
+    "recipe",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
