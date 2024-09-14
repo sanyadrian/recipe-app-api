c63d68fe9e39a46de511e437281ed1f59ff14963
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-14 17:20:25 -0400

    Cleaned up code in urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -31,3 +31,9 @@ urlpatterns = [
     path("api/user/", include("user.urls")),
     path("api/recipe/", include("recipe.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
