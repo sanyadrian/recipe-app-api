7d282db01022cf138a722f1a28a5267620fad5a7
sanyadrian <adrianovoleksandr@gmail.com>
2024-09-15 13:26:43 -0400

    added image to recipe detail serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -74,7 +74,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ["description"]
+        fields = RecipeSerializer.Meta.fields + ["description", "image"]
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
